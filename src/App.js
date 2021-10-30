941ca90c3b883b01b74068417ed4d9a469271af7
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-10-30 22:42:23 +0600

    Basic routing pages added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Destinations from './components/Destinations/Destinations';
+import Packages from './components/Packages/Packages';
+import ContactUs from './components/ContactUs/ContactUs';
+import Gallary from './components/Gallary/Gallary';
+import MyOrders from './components/MyOrders/MyOrders';
+import AllOrders from './components/AllOrders/AllOrders';
+import AddService from './components/AddService/AddService';
+import Login from './components/Login/Login';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -9,7 +19,36 @@ function App() {
       <Router>
         <Header />
         <Switch>
-
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/destinations">
+            <Destinations />
+          </Route>
+          <Route exact path="/packages">
+            <Packages />
+          </Route>
+          <Route exact path="/contacts">
+            <ContactUs />
+          </Route>
+          <Route exact path="/gallary">
+            <Gallary />
+          </Route>
+          <Route exact path="/myOrders">
+            <MyOrders />
+          </Route>
+          <Route exact path="/allOrders">
+            <AllOrders />
+          </Route>
+          <Route exact path="/addService">
+            <AddService />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
         </Switch>
       </Router>
 
