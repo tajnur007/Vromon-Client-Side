ac3a58d47d8430239dc4a007cb6d76411d83c6f8
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-01 22:24:05 +0600

    Destinations page complete

--- src/components/Destinations/Destinations.js
+++ src/components/Destinations/Destinations.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import Destination from '../Destination/Destination';
 
 const Destinations = () => {
-    return (
-        <div>
+    const [destinations, setDestinations] = useState([]);
 
+    useEffect(() => {
+        fetch('https://secret-coast-24933.herokuapp.com/destinations')
+            .then(resp => resp.json())
+            .then(data => setDestinations(data));
+    }, [])
+
+    return (
+        <div className="container my-5">
+            <h1 className="my-5">Select Our Best Popular <br /> Destinations</h1>
+            <Row xs={1} md={3} lg={4} className="g-4">
+                {
+                    destinations.map(data => <Destination key={data.key} value={data}></Destination>)
+                }
+            </Row>
         </div>
     );
 };
