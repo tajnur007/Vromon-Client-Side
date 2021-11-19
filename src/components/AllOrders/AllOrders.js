cf67f14be1a1c50c403ec2edb86960a3248f204e
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-19 22:17:16 +0600

    Manage all orders process complete

--- src/components/AllOrders/AllOrders.js
+++ src/components/AllOrders/AllOrders.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
+import AllOrdersData from './AllOrdersData';
 
 const AllOrders = () => {
-    return (
-        <div>
+    const { user } = useAuth();
+    const [orders, setOrders] = useState([]);
 
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders?email=${user.email}`)
+            .then(resp => resp.json())
+            .then(data => setOrders(data))
+
+    }, [user.email]);
+
+    return (
+        <div className="container my-5">
+            <h1 className="txt-primary pb-3">MANAGE ALL ORDERS</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Package Details</th>
+                        <th scope="col">Duration</th>
+                        <th scope="col">Receiver</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        orders.map(order => <AllOrdersData key={order._id} value={order} />)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
---
+++ src/components/AllOrders/AllOrdersData.js
@@ -0,0 +1,104 @@
+import React, { useRef, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+
+const AllOrdersData = (props) => {
+    const { packageDetails, duration, receiverName, address, status } = props.value;
+    const statusRef = useRef();
+
+    // Modal Controls 
+    const [showDelete, setShowDelete] = useState(false);
+    const handleDeleteClose = () => setShowDelete(false);
+    const handleDeleteShow = () => setShowDelete(true);
+
+    // Handle Order Cancel Method 
+    const handleUpdate = () => {
+        const url = `http://localhost:5000/updateOrder`;
+        const newOrder = { ...props.value };
+        newOrder.status = `${statusRef.current.value}`;
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newOrder)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.modifiedCount === 1) {
+                    alert('Update complete!');
+                }
+            })
+    }
+
+    // Handle Order Delete Method 
+    const handleDelete = () => {
+        const url = `http://localhost:5000/deleteOrder`;
+        const order = { ...props.value };
+        fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(order)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.deletedCount === 1) {
+                    handleDeleteClose();
+                }
+            })
+    }
+
+    return (
+        <tr>
+            <td>{packageDetails}</td>
+            <td>{duration} </td>
+            <td>{receiverName} </td>
+            <td>{address} </td>
+            <td>
+                <select ref={statusRef} class="form-select form-select-sm" id="floatingSelectGrid">
+                    <option defaultValue> {status} </option>
+                    <option value="Processing">Processing</option>
+                    <option value="Done">Done</option>
+                    <option value="Canceled">Canceled</option>
+                </select>
+            </td>
+            <td>
+
+                {/* Update Button  */}
+                <Button variant="success" onClick={handleUpdate} className="me-2">
+                    Update
+                </Button>
+
+                {/* Delete Button trigger modal  */}
+                <Button variant="danger" onClick={handleDeleteShow} className="me-2">
+                    Delete
+                </Button>
+
+                <Modal
+                    show={showDelete}
+                    onHide={handleDeleteClose}
+                    backdrop="static"
+                    keyboard={false}
+                    centered
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Are you sure?</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Delete operation will premanently delete this order from database. If you really want to delete this order then click Confirm Delete buton again, else Cancel.
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleDeleteClose}>
+                            Close
+                        </Button>
+                        <Button variant="danger" onClick={handleDelete}>Confirm Delete</Button>
+                    </Modal.Footer>
+                </Modal>
+            </td>
+        </tr>
+    );
+};
+
+export default AllOrdersData;
